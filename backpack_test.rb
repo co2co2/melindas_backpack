dd44ebefd2ae32a42e55f7e733c9715ecadba732
co2co2 <coco8287@hotmail.com>
2017-12-12 14:09:20 -0500

    changed variable name x to weather

--- backpack_test.rb
+++ backpack_test.rb
@@ -45,7 +45,12 @@ class BackpackTest < MiniTest::Test
 
   ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
   # is something missing here?
-
+  def test_that_packs_gym_shoes_on_mondays_and_thursdays
+    ['monday', 'thursday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      assert (backpack.items.include?('gym shoes'))
+    end
+  end
 
   ##### TESTS ABOUT PRINTING THE LIST #####
   def test_that_backpack_prints_correctly
